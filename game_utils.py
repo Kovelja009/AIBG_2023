cecda210819e6373d4c82bee979c9386d8894a7b
Ognjen <oneskovic0@gmail.com>
2023-12-10 00:30:09 +0100

    Add get_all_tiles_of_type to utils

---
+++ game_utils.py
@@ -0,0 +1,4 @@
+from game_state import TileType, EntityType, GameState
+
+def get_all_tiles_of_type(game_state: GameState, entity_type: EntityType):
+    return [tile for tile in game_state.tiles.values() if tile.entity.type == entity_type]
--- strategies/go_to_chest_strategy.py
+++ strategies/go_to_chest_strategy.py
@@ -1,5 +1,8 @@
 from strategy import Strategy
 from strategy_manager import StrategyManager
+from game_state import TileType, EntityType
+from game_utils import get_all_tiles_of_type
+import logging
 
 class GoToChestStrategy(Strategy):
     def __init__(self, manager: StrategyManager):
@@ -7,8 +10,13 @@ class GoToChestStrategy(Strategy):
     
     def execute(self, game_state):
         # Find chest position that corresponds to our player
-        chest = None
-        for tile in game_state.tiles.values():
-            if tile.entity.type == 6:
-                chest = tile
-                break
+        chests = get_all_tiles_of_type(game_state, EntityType.CHEST)
+        our_chest = None
+        try:
+            our_chest = [chest for chest in chests if chest.entity.idx == game_state.our_idx][0]
+        except IndexError:
+            logging.error("Could not find our chest")
+            return # TODO: Handle strategy failure
+
+        our_player = game_state.players[game_state.our_idx]
+
