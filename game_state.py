5d6febe3cbd32db59dab3b29a63241d0bb0a3ab8
Kovelja009 <vanjakovinic009@gmail.com>
2023-12-09 23:27:51 +0100

    initial commit

---
+++ game_state.py
@@ -0,0 +1,68 @@
+from enum import Enum
+from typing import List
+
+class TileType(Enum):
+    EMPTY = 0
+    FULL = 1
+
+class EntityType(Enum):
+    NONE = 0
+    TREES = 1
+    CHEST = 2
+    STONE = 3
+    CLIFF = 4
+    ENEMY_PLAYER = 5
+    SKULL = 6
+
+class Entity:
+    def __init__(self, entity_type, q, r, **kwargs):
+        self.type = entity_type
+        self.q = q
+        self.r = r
+        if kwargs:
+            self.__dict__.update(kwargs)
+
+class Tile:
+    def __init__(self, q, r, tile_type, entity):
+        self.q = q
+        self.r = r
+        self.tileType = tile_type
+        self.entity = Entity(**entity)
+
+
+class Player:
+    def __init__(self, type, q, r, playerIdx, name, score, health, attackPower, skull, skullWin, scoreLevel, sword) -> None:
+        self.type = type
+        self.q = q
+        self.r = r
+        self.playerIdx = playerIdx
+        self.name = name
+        self.score = score
+        self.health = health
+        self.attackPower = attackPower
+        self.skull = skull
+        self.skullWin = skullWin
+        self.scoreLevel = scoreLevel
+        self.sword = sword
+
+
+class GameState:
+    def __init__(self, turn, map_size, skull_win: bool, tiles: List[Tile], players: List[Player]):
+        self.turn = turn
+        self.size = map_size
+        self.tiles = tiles
+        self.skull_win = False
+        # go through players and check if q and r is the same as tiles q and r and set tiles entity to player
+        for player in players:
+            for tile in tiles:
+                if player.q == tile.q and player.r == tile.r:
+                    tile.entity = player
+                    break
+
+
+# # Creating GameState object from JSON data
+# game_state = GameState(
+#     json_data['gameState']['turn'],
+#     json_data['gameState']['map']['size'],
+#     [Tile(**tile) for tile in json_data['gameState']['map']['tiles']]
+# )
