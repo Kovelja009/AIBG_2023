36d5523f31e1df3fad6a75ae2e62bd917f60c350
Kovelja009 <vanjakovinic009@gmail.com>
2023-12-10 00:46:19 +0100

    added state for stone

--- game_state.py
+++ game_state.py
@@ -1,9 +1,11 @@
 from enum import Enum
 from typing import List, Dict
 
+
 class TileType(Enum):
     EMPTY = 0
     FULL = 1
+
 
 class EntityType(Enum):
     NONE = 0
@@ -14,6 +16,7 @@ class EntityType(Enum):
     ENEMY_PLAYER = 5
     SKULL = 6
 
+
 class Entity:
     def __init__(self, entity_type, q, r, **kwargs):
         self.type = entity_type
@@ -22,16 +25,19 @@ class Entity:
         if kwargs:
             self.__dict__.update(kwargs)
 
+
 class Tile:
     def __init__(self, q, r, tile_type, entity):
         self.q = q
         self.r = r
         self.tileType = tile_type
         self.entity = Entity(**entity)
+        self.is_attacked = False
 
 
 class Player:
-    def __init__(self, type, q, r, playerIdx, name, score, health, attackPower, skull, skullWin, scoreLevel, sword) -> None:
+    def __init__(self, type, q, r, playerIdx, name, score, health, attackPower, skull, skullWin, scoreLevel,
+                 sword) -> None:
         self.type = type
         self.q = q
         self.r = r
@@ -45,19 +51,25 @@ class Player:
         self.scoreLevel = scoreLevel
         self.sword = sword
 
+
 class GameState:
     turn: int
     size: int
-    tiles: Dict[(int,int), Tile]
+    tiles: Dict[(int, int), Tile]
     players: List[Player]
     skull_win: bool
     our_idx: int
-    def __init__(self, turn, map_size, skull_win: bool, tiles: List[Tile] , players: List[Player]):
+    stone_state: int
+    players: List[Player]
+
+    def __init__(self, turn, map_size, our_idx, skull_win: bool, tiles: List[Tile], players: List[Player],
+                 stone_attacks: List[(int, int)]):
         self.turn = turn
         self.size = map_size
         self.skull_win = skull_win
         self.tiles = tiles
-        self.our_idx = 0
+        self.our_idx = our_idx
+        self.players = players
         # go through players and check if q and r is the same as tiles q and r and set tiles entity to player
         for player in players:
             for tile in tiles:
@@ -67,6 +79,15 @@ class GameState:
 
         self.tiles = {(tile.q, tile.r): tile for tile in tiles}
 
+        for stone_attack in stone_attacks:
+            self.tiles[stone_attack].is_attacked = True
+        self.stone_state = 0
+
+    # if in state 0-5 it has one type of attacks
+    # if in state 6 it has another type of attacks
+    def change_stone_state(self):
+        self.stone_state += 1
+        self.stone_state %= 7
 
 # # Creating GameState object from JSON data
 # game_state = GameState(
