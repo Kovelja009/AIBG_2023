bf63a4d063269b8e8f8a22fd4fb3181bd84a799d
Pavle Sekešan <pavlesekesan@gmail.com>
2023-12-10 03:17:29 +0100

    movement file

---
+++ movement.py
@@ -0,0 +1,7 @@
+from game_state import Tile
+
+def move_to(source: tuple, dest: tuple) -> tuple:
+    raise NotImplementedError
+
+def distance(source: tuple, dest: tuple) -> int:
+    raise NotImplementedError
