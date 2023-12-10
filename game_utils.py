4b8b24bc22b4fb1b48e5e0954be58c82d015369c
Ognjen <oneskovic0@gmail.com>
2023-12-10 01:04:39 +0100

    Add some functions to utils

--- game_utils.py
+++ game_utils.py
@@ -1,4 +1,30 @@
-from game_state import TileType, EntityType, GameState
+from typing import Tuple
+from game_state import TileType, EntityType, GameState, Tile
 
 def get_all_tiles_of_type(game_state: GameState, entity_type: EntityType):
     return [tile for tile in game_state.tiles.values() if tile.entity.type == entity_type]
+
+def __node_dist(node1: Tuple[int,int], node2: Tuple[int,int]) -> int:
+    '''
+    Returns the distance between two nodes on hexagonal grid by runing a bfs from node1 to node2
+    '''
+    return 0
+
+def __check_move_is_safe(game_state: GameState, next_tile: Tile) -> bool:
+    '''
+    Check if moving to this tile results in being next to an enemy player
+    '''
+    enemy_players = [player for player in game_state.players if player.idx != game_state.our_idx]
+    for player in enemy_players:
+        q1,r1 = player.tile.q, player.tile.r
+        q2,r2 = next_tile.q, next_tile.r
+        if __node_dist((q1,r1), (q2,r2)) == 1:
+            return False
+    return True
+
+def try_execute_move(game_state: GameState, next_tile: Tile):
+    '''
+    Try to execute a move to a tile, if it is safe to do so
+    '''
+    if __check_move_is_safe(game_state, next_tile):
+        pass
--- strategy_manager.py
+++ strategy_manager.py
@@ -1,6 +1,10 @@
 from strategy import Strategy
 
 class StrategyManager:
-    current_state : Strategy
+    current_strategy : Strategy
     def __init__(self) -> None:
-        self.current_state = None
+        self.current_strategy = None
+
+    def transition(self, new_strategy: Strategy):
+        self.current_strategy = new_strategy
+        self.current_strategy.execute()
